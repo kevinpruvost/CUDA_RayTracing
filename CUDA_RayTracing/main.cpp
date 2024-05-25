cb8cc1cf8d1aa6b13f684e28cd7abd51695727e1
kevin.pruvost <pruvostkevin0@gmail.com>
2024-05-25 18:14:55 +0800

    final screenshot

--- CUDA_RayTracing/main.cpp
+++ CUDA_RayTracing/main.cpp
@@ -5,13 +5,14 @@ int main()
     Renderer renderer(1600, 900,
 //        3840, 2160,
         1920, 1080,
+//        640, 360,
         "scene_final.txt"
     );
     renderer.settings.depthOfField.enabled = true;
     renderer.settings.depthOfField.aperture = 70;
     renderer.settings.depthOfField.focalDistance = 168;
     //renderer.Render();
-    renderer.RenderInParallel("scene_final.bmp", 32, 70, 168);
+    renderer.RenderInParallel("scene_final.bmp", 128, 150, 168);
     //renderer.RenderInParallel("super_fine_one.bmp", 32, 1, 1.35);
     return 0;
 }
